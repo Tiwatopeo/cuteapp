26391b9bb5b512c0679ae0a85d8580c8a9aaa338
Tiwatopeo <90646064+Tiwatopeo@users.noreply.github.com>
2022-03-22 13:16:04 +0100

    Add files via upload

---
+++ school/home/app.js
@@ -0,0 +1,7 @@
+CREATE TABLE sport(
+my id SERIEAL PRIMARY KEY,
+sport name VARCHAR(50) UNIQUE NOT NULL,
+numbers of players VARCHAR(50)UNIQUE NOT NULL,
+type of sport  VARCHAR(50)UNIQUE NOT NULL,
+created_on_DATE NOT NULL
+);
